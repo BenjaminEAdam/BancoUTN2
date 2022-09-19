488179e74abd42b0334f93dfa8f1704d3e7bf824
benja <benjaadam14@gmail.com>
2022-09-18 23:16:43 -0300

    segundo commit diseño de la primer pantalla terminada

--- app/src/main/java/primeraPractica/nombreGrupo/MainActivity.java
+++ app/src/main/java/primeraPractica/nombreGrupo/MainActivity.java
@@ -4,11 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+        Spinner spinnerMoneda=findViewById(R.id.spinner);
+
+        //ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this, R.array.moneda, android.R.layout.simple_spinner_item);
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, getResources().getStringArray(R.array.moneda));
+
+        spinnerMoneda.setAdapter(adapter);
     }
+
 }
